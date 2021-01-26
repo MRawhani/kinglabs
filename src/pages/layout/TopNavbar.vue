40357bd3305276b891681b213d7ca3d3d19861ce
m-alghobary <m.alghobary90@gmail.com>
2021-01-26 19:37:58 +0300

    make top navbar works

--- src/pages/layout/TopNavbar.vue
+++ src/pages/layout/TopNavbar.vue
@@ -1,7 +1,34 @@
 <template>
-	<div>
-		<p>TopBar</p>
-	</div>
+	<v-app-bar app fixed color="white">
+		<div class="d-flex align-center justify-space-between d-block top-bar">
+			<v-toolbar-title class="text-h5 ms-2">كينج لابز</v-toolbar-title>
+			<div>
+				<v-menu offset-y transition="scale-transition">
+					<template v-slot:activator="{ on }">
+						<v-btn icon medium outlined depressed color="primary">
+							<v-avatar v-on="on">M</v-avatar>
+						</v-btn>
+					</template>
+					<v-list nav dense>
+						<v-list-item-group color="primary">
+							<v-list-item>
+								<v-list-item-icon>
+									<v-icon>mdi-account-box-outline</v-icon>
+								</v-list-item-icon>
+								<v-list-item-title>الملف الشخصي</v-list-item-title>
+							</v-list-item>
+							<v-list-item to="/logout">
+								<v-list-item-icon>
+									<v-icon>mdi-logout-variant</v-icon>
+								</v-list-item-icon>
+								<v-list-item-title>تسجيل الخروج</v-list-item-title>
+							</v-list-item>
+						</v-list-item-group>
+					</v-list>
+				</v-menu>
+			</div>
+		</div>
+	</v-app-bar>
 </template>
 
 <script>
@@ -9,4 +36,8 @@ export default {
 	name: 'TopNavbar',
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.top-bar {
+	width: 100%;
+}
+</style>
