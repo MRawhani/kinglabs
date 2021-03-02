883f7caf39f01c2c1cf7d1a87d644add050ced4b
Rawhani <mohammedrawhani@gmail.com>
2021-03-03 02:38:29 +0300

    adding own reo

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
         publish: [
           {
             provider: "github",
-            owner: "m-alghobary",
+            owner: "mrawhani",
             
           }
         ]
